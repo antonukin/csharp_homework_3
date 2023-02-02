f4989f894cf392d345001db85d045d02dcf1684f
Anton <antonukin@icloud.com>
2023-02-02 16:04:23 +0300

    task_2_1 added

---
+++ task_2_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+double GetNumber(string message)
+{
+double result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+            if(double.TryParse(Console.ReadLine(), out result))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Введено не число. Повторите ввод.");
+            }
+    }
+    return result;
+}
+
+double firstDotX = GetNumber("Введите первую координату по оси Х");
+double firstDotY = GetNumber("Введите первую координату по оси Y");
+double firstDotZ = GetNumber("Введите первую координату по оси Z");
+double secondDotX = GetNumber("Введите вторую координату по оси Х");
+double secondDotY = GetNumber("Введите вторую координату по оси Y");
+double secondDotZ = GetNumber("Введите вторую координату по оси Z");
+
+double dotDistanceX = Math.Pow((secondDotX - firstDotX), 2);
+double dotDistanceY = Math.Pow((secondDotY - firstDotY), 2);
+double dotDistanceZ = Math.Pow((secondDotZ - firstDotZ), 2);
+double dotDistance = Math.Sqrt(dotDistanceX + dotDistanceY + dotDistanceZ);
+dotDistance = Math.Round(dotDistance, 2);
+
+Console.WriteLine($"Расстоянием между точками ({firstDotX}, {firstDotY}, {firstDotZ}) и ({secondDotX}, {secondDotY}, {secondDotZ}) является {dotDistance}");
